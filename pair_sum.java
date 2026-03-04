fd1810f30b6a79778ed3c0dab4dd8a5c199b91fb
Bisma Raza <bismauniversity@gmail.com>
2026-03-04 22:07:05 +0500

    Two Sum

--- pair_sum.java
+++ pair_sum.java
@@ -1,3 +1,44 @@
+import java.util.Vector;
+import java.util.*;
+
 public class pair_sum {
-    
+    // Brute force Approach
+    public static int Pairsum(int[] arr, int target) {
+        int n = arr.length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) {
+                    ans = arr[i] + arr[j];
+                    return ans;
+                }
+            }
+        }
+        return -1;
+    }
+    // Optimized Approach O(n)
+    public static int optimized(int arr[], int target) {
+        int n = arr.length;
+        int start = 0;
+        int end = n - 1;
+        while (start < end) {
+            int ans = arr[start] + arr[end];
+            if (ans > target) {
+                end--;
+            } else if (ans < target) {
+                start++;
+            } else {
+                 System.out.println(arr[start] + " + " + arr[end] + " = " + ans);
+            return ans;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 7, 11, 15 };
+        int target = 26;
+        optimized(arr, target);
+
+    }
 }
