31d9df9d855fc67830d9b1f2546a82bbb9fa3b36
Bisma Raza <bismauniversity@gmail.com>
2026-03-05 20:52:43 +0500

    HashSet

--- hashing.java
+++ hashing.java
@@ -1,3 +1,21 @@
+import java.util.*;
+
 public class hashing {
-    
+
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        hm.put("Pakistan", 150);
+        hm.put("US", 7);
+        hm.put("VS", 5);
+        hm.put("Nepal", 2);
+        hm.put("Poland", 10);
+
+        // Iterate
+        Set<String> keys = hm.keySet();
+        System.out.println(keys);
+
+    }
+
 }
