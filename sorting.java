662c582042b02d5701cc8ae28ffcc39ee7646616
Bisma Raza <bismauniversity@gmail.com>
2026-03-11 20:01:45 +0500

    Sorting

--- sorting.java
+++ sorting.java
@@ -1,3 +1,27 @@
 public class sorting {
     
+    public static void insertionSort(int arr[],int n){
+        for(int i = 1; i<n; i++){
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev >= 0 && arr[prev] < curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+    }
+
+    public static void print(int arr[], int n){
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={6,1,6,3,5};
+        int n = arr.length;
+
+        insertionSort(arr, n);
+        print(arr, n);
+    }
 }
