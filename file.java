15613dd9eec00dd5a23dc952fbd0993c12d2fbab
Bisma Raza <bismauniversity@gmail.com>
2026-04-13 23:02:15 +0500

    code

---
+++ file.java
@@ -0,0 +1,10 @@
+
+
+
+class file {
+    public static void main(String[] args) {
+        // File name specified
+        file obj = new file("myfile.txt");
+        System.out.println("File Created!");
+    }
+}
